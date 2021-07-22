a92b0ef36f950bf895562e79bba7b8dba9990775
Martin Völkl <martin@madst.de>
2021-07-22 19:19:28 +0200

    company Logo

---
+++ IMG_77122.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="IMG_77122" tilewidth="32" tileheight="32" tilecount="84" columns="21">
+ <image source="IMG_77122.png" width="689" height="141"/>
+</tileset>
